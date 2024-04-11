c8ec5b24bf1a8bf8248c732e93989890db58be70
matheoleger <matheo.leger@ynov.com>
2024-04-11 16:53:30 +0200

    feat(0.5.0): Change URL for database

--- user-service/app.py
+++ user-service/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Connect to MongoDB
-client = MongoClient('mongodb://mongodb:27017')
+client = MongoClient('mongodb://mongo:27017')
 db = client['userdb']
 collection = db['user']
 
